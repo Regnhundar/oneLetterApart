62447983267c26cc9a2ed5dcccc15ef2e74f5877
Magnus Nilsson <regnhundar@gmail.com>
2024-01-08 11:37:43 +0100

    fix

---
+++ scripts/script.js
@@ -0,0 +1,45 @@
+'use strict'; // Påminner om användandet av bla semikolon.
+
+console.log(`Hello world`)
+
+let startWord = `FOUR`;
+let endWord = `FIVE`;
+let isInputApproved = false;
+let counter = 0;
+let dictionary = [`FOUR`, `FOUL`, `FOOL`, `FOOT`, `FORT`, `FORE`, `FIRE`, `FIVE`];
+
+while (startWord !== endWord) {
+    while (!isInputApproved) {
+        let newWord = window.prompt(`Nuvarande ord: ` + startWord +
+            `. Antal gissningar: ` + counter +
+            `. Vänligen ange det nya ordet!`);
+        if (newWord.length !== startWord.length) {
+            window.alert(`Ditt ord måste innehålla lika många bokstäver som startordet, dumbass!`)
+        } else if (!dictionary.includes(newWord)) {
+            window.alert(`Ordet finns för tusan inte i ordlistan, pucko!`);
+
+        } else if (!isOneLetterApart(newWord, startWord)) {
+            window.alert(`Du får bara byta ut en bokstav!`);
+        } else {
+            isInputApproved = true;
+            startWord = newWord;
+            counter++;
+        }
+    }
+    isInputApproved = false;
+}
+
+function isOneLetterApart(newWord, startWord) {
+    let diffCount = 0;
+
+    for (let i = 0; i < startWord.length; i++) {
+        if (newWord[i] !== startWord[i]) {
+            diffCount++
+            // diffCount = diffCount + 1;
+            //diffCount += 1;
+        }
+    }
+    return diffCount === 1; // Sant om diffCount är 1, annars falskt
+}
+
+console.log(`Grattis du vann!`)
